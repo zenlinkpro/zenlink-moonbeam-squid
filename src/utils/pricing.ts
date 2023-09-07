d212c9c4765b5c634ea9110a6972161e87a0dad2
Hayden Fan <haosong0323@gmail.com>
2023-09-07 20:13:59 +0800

    feat: migrate to arrowSquid

--- src/utils/pricing.ts
+++ src/utils/pricing.ts
@@ -1,18 +1,20 @@
-import { CommonHandlerContext } from "@subsquid/substrate-processor"
-import { Store } from "@subsquid/typeorm-store"
 import { Big as BigDecimal } from 'big.js'
 import { ONE_BD, ZERO_BD } from "../consts"
 import { getPair } from "../entities/pair"
 import { getOrCreateToken } from "../entities/token"
 import { Pair, StableSwap } from "../model"
+import { Context, Log } from "../processor"
 
 export const WNATIVE = '0xAcc15dC74880C9944775448304B263D191c6077F'.toLowerCase()
 export const USDC = '0x818ec0A7Fe18Ff94269904fCED6AE3DaE6d6dC0b'.toLowerCase()
+export const FRAX = '0x322E86852e492a7Ee17f28a78c663da38FB33bfb'.toLowerCase()
 export const WNATIVE_USDC = '0x43182F87401cc95904117eB0188E3D0d8ACCA430'.toLowerCase()
+export const WNATIVE_FRAX = '0x79c5b0A746fC7B08461BF007281C5f0A313DA692'.toLowerCase()
 
 export const WHITELIST: string[] = [
   '0xAcc15dC74880C9944775448304B263D191c6077F'.toLowerCase(), // wnative
   '0x818ec0A7Fe18Ff94269904fCED6AE3DaE6d6dC0b'.toLowerCase(), // usdc
+  '0x322E86852e492a7Ee17f28a78c663da38FB33bfb'.toLowerCase(), // frax
   '0x3Fd9b6C9A24E09F67b7b706d72864aEbb439100C'.toLowerCase(), // zlk
 ]
 
@@ -22,13 +24,21 @@ export const MINIMUM_USD_THRESHOLD_NEW_PAIRS = new BigDecimal(50)
 // minimum liquidity for price to get tracked
 export const MINIMUM_LIQUIDITY_THRESHOLD_ETH = new BigDecimal(5)
 
-export async function getEthPriceInUSD(ctx: CommonHandlerContext<Store>): Promise<BigDecimal> {
+export async function getEthPriceInUSD(ctx: Context): Promise<BigDecimal> {
   const usdcPair = await getPair(ctx, WNATIVE_USDC)
-  if (!usdcPair) return BigDecimal(0)
+  const fraxPair = await getPair(ctx, WNATIVE_FRAX)
 
-  return usdcPair.token0.id === USDC
-    ? BigDecimal(usdcPair.token0Price)
-    : BigDecimal(usdcPair.token1Price)
+  if (fraxPair) {
+    return fraxPair.token0.id === FRAX
+      ? BigDecimal(fraxPair.token0Price)
+      : BigDecimal(fraxPair.token1Price)
+  } 
+  if (usdcPair) {
+    return usdcPair.token0.id === USDC
+      ? BigDecimal(usdcPair.token0Price)
+      : BigDecimal(usdcPair.token1Price)
+  }
+  return BigDecimal(0)
 }
 
 /**
@@ -36,7 +46,8 @@ export async function getEthPriceInUSD(ctx: CommonHandlerContext<Store>): Promis
  * @todo update to be derived ETH (plus stablecoin estimates)
  * */
 export async function findEthPerToken(
-  ctx: CommonHandlerContext<Store>,
+  ctx: Context,
+  log: Log,
   tokenId: string
 ): Promise<BigDecimal> {
   if (tokenId === WNATIVE) {
@@ -58,11 +69,11 @@ export async function findEthPerToken(
   for (const pair of whitelistPairs) {
     if (BigDecimal(pair.reserveETH).gt(MINIMUM_LIQUIDITY_THRESHOLD_ETH)) {
       if (pair.token0.id === tokenId) {
-        const token1 = await getOrCreateToken(ctx, pair.token1.id)
+        const token1 = await getOrCreateToken(ctx, log, pair.token1.id)
         return BigDecimal(pair.token1Price).mul(token1.derivedETH) // return token1 per our token * Eth per token 1
       }
       if (pair.token1.id === tokenId) {
-        const token0 = await getOrCreateToken(ctx, pair.token0.id)
+        const token0 = await getOrCreateToken(ctx, log, pair.token0.id)
         return BigDecimal(pair.token0Price).mul(token0.derivedETH) // return token0 per our token * ETH per token 0
       }
     }
@@ -71,10 +82,11 @@ export async function findEthPerToken(
 }
 
 export async function findUSDPerToken(
-  ctx: CommonHandlerContext<Store>,
+  ctx: Context,
+  log: Log,
   tokenId: string
 ): Promise<BigDecimal> {
-  const tokenUSDPrice = (await getEthPriceInUSD(ctx)).mul(await findEthPerToken(ctx, tokenId))
+  const tokenUSDPrice = (await getEthPriceInUSD(ctx)).mul(await findEthPerToken(ctx, log, tokenId))
   if (tokenUSDPrice.eq(ZERO_BD)) {
     // check for stableSwap lpToken
     const stableSwap = await ctx.store.findOneBy(StableSwap, {
@@ -85,7 +97,7 @@ export async function findUSDPerToken(
       let USDPrice = BigDecimal('0')
       for (const token of tokens) {
         if (USDPrice.gt(ZERO_BD)) break
-        USDPrice = await findUSDPerToken(ctx, token)
+        USDPrice = await findUSDPerToken(ctx, log, token)
       }
       return USDPrice
     }
